01782718daad600f03fac6055bf7907edd78929d
Shyrro <zsahmane@gmail.com>
2021-02-02 20:29:52 +0100

    Correction exo 1
    Signed-off-by: Shyrro <zsahmane@gmail.com>

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Exo1 />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Exo1 from './components/Exo1.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Exo1
   }
 })
 </script>
---
+++ my-vue-app/src/components/Exo1.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <input type="text" v-model="message">
+    <span v-if="showMessage"> {{ message }} </span>
+    <ul v-else>
+      <li v-for="item in collection" :key="item.id">{{ item.value}}</li>
+    </ul>
+    <button @click="toggleMessage">Toggle</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  data() {
+    return {
+      showMessage: true,
+      message: 'Mon message',
+      collection: [
+        {id:0, value:'ananas'},
+        {id:1, value:'pomme'},
+        {id:2, value:'poire'},
+        {id:3, value:'pasthèque'}
+      ]
+    }
+  },
+  methods: {
+    toggleMessage() {
+      this.showMessage = !this.showMessage;
+    }
+  }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
